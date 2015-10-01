824d7a966e5c3cd5537c9e360a1a593aff9318d1
Phil DiMarco <plantlife26@gmail.com>
2015-09-30 20:16:30 -0400

    slight change

--- src/gasStationControlPackage/MessageGenerator.java
+++ src/gasStationControlPackage/MessageGenerator.java
@@ -5,9 +5,10 @@ import messagePackage.*;
 public class MessageGenerator {
 	
 public Message generateMessage(String s) throws Exception{
+	String temp;
 	if(s.contains("CreditCardReader")){
-	String a = s.replace("CreditCardReader ", "");
-	return new CreditCardReader(a);
+	temp = s.replace("CreditCardReader ", "");
+	return new CreditCardReader(temp);
 	}
 	else throw new Exception("invalid message requested");
 }
